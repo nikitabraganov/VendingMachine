e6029be6aac0b4ba1ee249228db5b27ccb7516b9
DESKTOP-NIPST61\Nikita <nikita.braganov@gmail.com>
2017-11-26 15:58:35 +0200

    Tema pentru acasa

---
+++ tema1/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSimpleProgram
+{
+    class Animal
+    {
+        public string name = "";
+        public string sound = "";
+
+
+        public virtual void speak()
+        {
+            Console.WriteLine(name + " says " + sound);
+        }
+
+    }
+    class Cat : Animal
+    {
+
+
+        public Cat()
+        {
+            name = "Cat";
+            sound = "Meow";
+        }
+        public override void speak()
+        {
+            Console.WriteLine(name + " says " + sound);
+        }
+
+    }
+    class Horse : Animal
+    {
+        public Horse()
+        {
+            name = "Horse";
+            sound = "Pfffrt";
+        }
+        public override void speak()
+        {
+            Console.WriteLine(name + " says " + sound);
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            name = "Dog";
+            sound = "Bark";
+        }
+        public override void speak()
+        {
+            Console.WriteLine(name + " says " + sound);
+        }
+    }
+
+}
