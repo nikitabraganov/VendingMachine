ee230dee3b0923fd2f1a9fbb7fa7b185612bb035
DESKTOP-NIPST61\Nikita <nikita.braganov@gmail.com>
2017-11-27 13:08:39 +0200

    Update

---
+++ tema1/AnimalSimpleProject/AnimalSimpleProject/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSimpleProject
+{
+    class Animal
+    {
+
+        protected string name = "";
+        protected string sound = "";
+
+
+        public virtual void speak()
+        {
+            Console.WriteLine(name + " says " + sound);
+
+        }
+    }
+}
---
+++ tema1/AnimalSimpleProject/AnimalSimpleProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSimpleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal firtsAnimal = new Dog();
+            Animal secondAnimal = new Cat();
+            Animal thirdAnimal = new Horse();
+            List < Animal > list = new List<Animal>();
+            list.Add(firtsAnimal);
+            list.Add(secondAnimal);
+            list.Add(thirdAnimal);
+            
+                        foreach (Animal a in list)
+            {
+                a.speak();
+                           }
+            Console.ReadKey();
+        }
+    }
+}
